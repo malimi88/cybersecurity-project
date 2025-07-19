5125b56795091be1fa84305563ce0e208bcbd3e9
malimi88 <malimie190@gmail.com>
2025-07-19 14:27:36 +0300

    Update test_scanner.py

--- ests/test_scanner.py
+++ ests/test_scanner.py
@@ -1,1 +1,10 @@
+import unittest
+from modules import scanner
 
+class TestScanner(unittest.TestCase):
+    def test_run_scan(self):
+        # This is a placeholder test
+        self.assertIsNone(scanner.run_scan("localhost"))
+
+if __name__ == '__main__':
+    unittest.main()
